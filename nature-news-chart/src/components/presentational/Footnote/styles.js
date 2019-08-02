9f6757a7d2c8449992bfc51ff1c8a2c84732c8fb
Chris Ryan <c.ryan@nature.com>
2019-08-02 12:26:21 +0100

    Adds line height for footnote

--- nature-news-chart/src/components/presentational/Footnote/styles.js
+++ nature-news-chart/src/components/presentational/Footnote/styles.js
@@ -1,7 +1,8 @@
 import styled from "@emotion/styled"
 
-const StyledFootnote = styled.header`
+const StyledFootnote = styled.p`
 	font-size: ${props => props.theme.fontSize.s};
+	line-height: ${props => props.theme.fontSize.normal};
 	padding: 0;
 	margin: 0;
 `
